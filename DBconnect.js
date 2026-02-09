82537adda300122c965e7c1032aad0d6a561b79a
Saurav-78 <saurav.pandit024@gmail.com>
2026-02-08 18:51:26 -0800

    updated commit

--- DBconnect.js
+++ DBconnect.js
@@ -1,13 +1,12 @@
 //mongodb+srv://sauravpandit024_db_user:WhdB932LFIppgd3k@cluster0.j1yu1ic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 const mongoose = require('mongoose');
 const url = "mongodb+srv://sauravpandit024_db_user:WhdB932LFIppgd3k@cluster0.j1yu1ic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const connect = ()=>{mongoose.connect(url,{
-     useNewUrlParser:true, 
-useUnifiedTopology:true
-}
-      
-)
-.then(()=>console.log("mongoDB connected succsesfully"))
-.catch((err)=>console.error("mongoDB connection error",err))
+const connect = async()=>{
+     try{
+          await mongoose.connect(url);
+             console.log('mongoDB connected succsesfully')
+     }
+     catch(err)
+   {console.error("mongoDB connection error",err)}
 }
 module.exports = connect;
